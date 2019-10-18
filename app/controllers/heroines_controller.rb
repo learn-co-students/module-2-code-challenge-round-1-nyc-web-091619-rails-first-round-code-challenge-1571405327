40769b6721a319e421e019ec37c717635db024b6
Connor Meth <connormeth27@gmail.com>
2019-10-18 11:05:58 -0400

    connor mod 2 code challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,36 @@
 class HeroinesController < ApplicationController
+  
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @powers = Power.all
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to(@heroine)
+    else 
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  # def set_heroine
+  #   @heroine = Heroine.find(params[:id])
+  # end
+
+
 end
