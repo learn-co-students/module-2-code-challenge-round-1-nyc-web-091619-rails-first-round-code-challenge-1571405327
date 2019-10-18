da08c3795345bafa9ab0264de1d51b1791c2b834
Altaf Quadri <altafmquadri@gmail.com>
2019-10-18 10:57:16 -0400

    Most basic functionality built

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,37 @@
 class HeroinesController < ApplicationController
+
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to heroines_path
+    else
+      redirect_to new_heroine_path, :flash => { :error => "Can't have the same name Heroine! Be original" }
+      #render :new, :flash => { :error => "Can't have the same name Heroine! Be original" }
+    end
+
+  end
+
+
+
+  
+  private #private methods
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,12 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+    validates :super_name, presence: true, uniqueness: true
+
+    def my_power
+        self.power
+    end
+
+    
+
+
 end
