0c507ab734289450b3550525e8a9ef77d7a29a79
maryfarner18 <maryfarner@gmail.com>
2019-10-18 10:00:57 -0400

    main functionality of challenge complete

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,34 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @power = @heroine.power
+    @power_name = @power.name
+    @super_name = @heroine.super_name
+    @name = @heroine.name
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+
+  private 
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,19 @@
 class PowersController < ApplicationController
+  
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+    @name = @power.name
+    @description = @power.description
+  end
+
+  private 
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
 end
