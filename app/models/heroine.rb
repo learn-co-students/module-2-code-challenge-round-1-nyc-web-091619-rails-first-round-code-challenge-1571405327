a0596dc3955591238ef5f8e40f0ca76c7bd3e11a
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-10-18 10:16:29 -0400

    completed all deliverables final

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -2,4 +2,6 @@ class Heroine < ApplicationRecord
     belongs_to :power
 
     validates :super_name, uniqueness: true
+    validates :name, presence: true
+    validates :power_id, presence: true
 end
