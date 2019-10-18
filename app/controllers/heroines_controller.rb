181a7a9c4b7caefd678f1e1f2a032e604a6ba109
jz0001 <jinming.zhang.92@gmail.com>
2019-10-18 10:54:49 -0400

    second commit with bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,11 +1,10 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    @heroines = Heroine.search(params[:search])
   end
 
   def show
     @heroine = Heroine.find(params[:id])
-
   end
 
   def new
@@ -22,12 +21,14 @@ class HeroinesController < ApplicationController
     else
       redirect_to new_heroine_path
     end
+
   end
 
 
   private
 
   def heroine_params
-    params.require(:heroine).permit(:name, :super_name, :power_id)
+    params.require(:heroine).permit(:name, :super_name, :power_id, :search)
   end
+
 end
