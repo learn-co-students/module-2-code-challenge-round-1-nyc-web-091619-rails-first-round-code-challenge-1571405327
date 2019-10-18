0e627492c8ca436b0e6e485951f13f69cbfcbab0
Michelle Lee <mlee092@Michelles-MacBook.local>
2019-10-18 10:59:50 -0400

    Mod 2 CC

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,49 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new  
+    @powers = Power.all
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def edit
+    @heroine = Heroine.find(params[:id])
+    @powers = Power.all
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id])
+    @heroine.update(heroine_params)
+    redirect_to heroine_path(@heroine)
+  end
+
+  def destroy
+    @heroine = Heroine.find(params[:id])
+    @heroine.destroy
+    redirect_to new_heroine_path
+  end
+
+  private
+  
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,46 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def new
+    @power = Power.new
+  end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def create
+    @power = Power.create(power_params)
+    if @power.save
+      redirect_to power_path(@power)
+    else
+      flash[:errors] = @power.errors.full_messages
+      redirect_to new_power_path
+    end
+
+  end
+
+  def edit
+    @power = Power.find(params[:id])
+  end
+
+  def update
+    @power = Power.find(params[:id])
+    @power.update(power_params)
+    redirect_to power_path(@power)
+  end
+
+  def destroy
+    @power = Power.find(params[:id])
+    @power.destroy
+    redirect_to new_power_path
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
 end
