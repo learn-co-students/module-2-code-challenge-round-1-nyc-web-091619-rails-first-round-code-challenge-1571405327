ecd85290aedde2041cb9f06122d80b5523b584f9
Jonathan Tan <you@example.com>
2019-10-18 11:06:25 -0400

    deliverable

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -3,4 +3,19 @@ class Heroine < ApplicationRecord
 
 
    validates_uniqueness_of :super_name
+
+
+
+   def self.search(user_input)
+        if user_input
+            @superpower = Power.find_by(name: user_input)
+       found = self.all.find_by(power: @superpower.id)
+            found.each do |heroine|
+                puts heroine.name 
+            end 
+        else 
+          self.all
+        end 
+   end 
+
 end
